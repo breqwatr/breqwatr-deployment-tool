2980dc66e3a2aa7fbd220b3237b91a42d644e9e9
kyle.pericak <kyle.pericak@breqwatr.com>
2019-09-12 13:45:23 -0400

    made setup tools

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""Set up Breqwatr Deployment Tool package"""
+from setuptools import setup
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+requirements = []
+with open('requirements.txt', 'r') as reqs_file:
+    requirements = reqs_file.read().splitlines()
+
+setup(
+    name='breqwatr-deployment-tool',
+    packages=['bwdt'],
+    version='1.00',
+    license='',
+    description='Deploy and manage Breqwatr services',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Kyle Pericak',
+    author_email='kyle@breqwatr.com',
+    url='https://github.com/breqwatr/breqwatr-deployment-tool',
+    keywords=['Breqwatr'],
+    install_requires=[
+        'click'
+    ],
+    entry_points='''
+        [console_scripts]
+        bwdt=bwdt.cli.main:main
+    ''',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ]
+)
