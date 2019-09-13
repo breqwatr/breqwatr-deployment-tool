cca0d89f05018439a82000a228c6dcabe0757e7a
kyle.pericak <kyle.pericak@breqwatr.com>
2019-09-12 22:59:08 -0400

    roughed in the other services

--- bwdt/lib.py
+++ bwdt/lib.py
@@ -2,6 +2,15 @@
 import os
 
 
+def get_latest_tag(image):
+    """ Returns the latest tag for a particular image supported by bwdt """
+    tags = {
+        'registry': '2',
+        'breqwatr/pxe': '7.0'
+    }
+    return tags[image]
+
+
 def _env_get(env_name, default_val):
     """ Return an environment variable if defined else default value """
     if env_name in os.environ:
@@ -15,3 +24,4 @@ def env():
         'auth_file': _env_get('BWDT_AUTH_FILE', '/etc/breqwatr/auth.json'),
         'region': _env_get('BWDT_REGION', 'ca-central-1'),
     }
+
--- bwdt/services.py
+++ bwdt/services.py
@@ -1,16 +1,116 @@
 """ Controls for the Breqwatr services """
 
 from bwdt.container import Docker
+from bwdt.lib import get_latest_tag
 
 
 def registry_start(ip='0.0.0.0', port=5000):
     """ Start the registry container """
     repo = 'registry'
-    tag = '2'
-    image = '{}:{}'.format(repo, tag)
+    tag = get_latest_tag(repo)
     docker = Docker()
     docker.pull(repository=repo, tag=tag)
     http_addr = "{}:{}".format(ip, port)
+    image = '{}:{}'.format(repo, tag)
     env = {'REGISTRY_HTTP_ADDR': http_addr}
-    success = docker.run(image, name='registry', environment=env)
+    ports = {'5000': '5000'}
+    success = docker.run(image, name='registry', environment=env, ports=ports)
     return success
+
+
+def pxe_start(uplink_interface, pxe_interface, dhcp_start, dhcp_end,
+              dhcp_subnet, listen_ip, dns_ip='8.8.8.8', tag=None):
+    """ Start the PXE container """
+    repo = "breqwatr/pxe"
+    if tag is None:
+        tag = get_latest_tag(repo)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    image = '{}:{}'.format(repo, tag)
+    env = {
+        'NAT_UPLINK': uplink_interface,
+        'INTERFACE': pxe_interface,
+        'DHCP_RANGE_START': dhcp_start,
+        'DHCP_RANGE_END': dhcp_end,
+        'DHCP_RANGE_SUBNET': dhcp_subnet,
+        'ENABLE_ROUTER': 'true',
+        'PXE_IP': listen_ip,
+        'DNS_IP': dns_ip
+    }
+    sysctls = {'net.ipv4.ip_forward': 1}
+    # TODO: Volumes
+    success = docker.run(image, name='bw-pxe', environment=env,
+                         sysctls=sysctls, privileged=True, network_mode='host')
+    return success
+
+
+def apt_start(tag=None, passkey=None):
+    """ Start the APT container """
+    repo = 'breqwatr/apt'
+    if tag is None:
+        tag = get_latest_tag(repo)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    image = '{}:{}'.format(repo, tag)
+    restart_policy = {'Name': 'always'}
+    if passkey is None:
+        passkey = 'FfMm5s3a'
+    env = {
+        'GPG_PASSKEY': passkey,
+        'GPG_PRIVATE_KEY_FILE': '/keys/breqwatr-private-key.asc',
+        'GPG_PUBLIC_KEY_FILE': '/keys/breqwatr-private-key.asc',
+    }
+    ports = {'80': ('0.0.0.0', '81')}
+    docker.run(image, name='bw-apt', environment=env,
+               restart_policy=restart_policy, ports=ports)
+
+
+def pip_start(tag=None):
+    """ Start the PIP container """
+    repo = 'breqwatr/pip'
+    if tag is None:
+        tag = get_latest_tag(repo)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    image = '{}:{}'.format(repo, tag)
+    docker.run(image, name='bw-pip', network_mode='host')
+
+
+def ansible_start(tag=None):
+    """ Start the Ansible container """
+    repo = 'breqwatr/ansible'
+    if tag is None:
+        tag = get_latest_tag(repo)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    image = '{}:{}'.format(repo, tag)
+    env = {'BW_GLOBALS_FILE': '/etc/breqwatr/globals.yml'}
+    # TODO: Volumes
+    docker.run(image, name='bw-ansible', network_mode='host', environment=env)
+
+
+def dns_start(interface_name, cloud_vip, cloud_fqdn, tag=None):
+    """ Start the DNS container """
+    repo = 'breqwatr/dns'
+    if tag is None:
+        tag = get_latest_tag(repo)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    image = '{}:{}'.format(repo, tag)
+    env = {
+        'INTERFACE': interface_name,
+        'CLOUD_VIP': cloud_vip,
+        'CLOUD_FQDN': cloud_fqdn,
+    }
+    docker.run(image, name='bw-dns', network_mode='host', environment=env)
+
+
+def ntp_start(tag=None):
+    """ Start the NTP service """
+    repo = 'breqwatr/ntp'
+    if tag is None:
+        tag = get_latest_tag(repo)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    image = '{}:{}'.format(repo, tag)
+    docker.run(image, name='bw-ntp', network_mode='host', privileged=True)
