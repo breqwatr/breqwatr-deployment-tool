d9e129b74be3912d1593c4feea33ad3a107f96a4
kyle.pericak <kyle.pericak@breqwatr.com>
2019-09-16 16:40:16 -0400

    renamed some services

--- bwdt/services.py
+++ bwdt/services.py
@@ -8,24 +8,23 @@ def registry_start(ip='0.0.0.0', port=5000):
     """ Start the registry container """
     repo = 'registry'
     tag = get_latest_tag(repo)
-    docker = Docker()
-    docker.pull(repository=repo, tag=tag)
     http_addr = "{}:{}".format(ip, port)
     image = '{}:{}'.format(repo, tag)
     docker_kwargs = {
         'environment': {'REGISTRY_HTTP_ADDR': http_addr},
         'ports': {'5000': '5000'}
     }
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
     success = docker.run(image, name='registry', **docker_kwargs)
     return success
 
 
 def pxe_start(interface, dhcp_start, dhcp_end, dns_ip='8.8.8.8'):
     """ Start the bw-pxe container """
+    name = 'breqwatr_pxe'
     repo = "breqwatr/pxe"
     tag = get_latest_tag(repo)
-    docker = Docker()
-    docker.pull(repository=repo, tag=tag)
     image = '{}:{}'.format(repo, tag)
     docker_kwargs = {
         'privileged': True,
@@ -38,17 +37,17 @@ def pxe_start(interface, dhcp_start, dhcp_end, dns_ip='8.8.8.8'):
         },
         'sysctls': {'net.ipv4.ip_forward': 1}
     }
-    success = docker.run(image, name='bw-pxe', **docker_kwargs)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    success = docker.run(image, name=name, **docker_kwargs)
     return success
 
 
 def apt_start(tag=None, passkey=None):
     """ Start the APT container """
+    name = 'breqwatr_apt'
     repo = 'breqwatr/apt'
-    if tag is None:
-        tag = get_latest_tag(repo)
-    docker = Docker()
-    docker.pull(repository=repo, tag=tag)
+    tag = get_latest_tag(repo)
     image = '{}:{}'.format(repo, tag)
     restart_policy = {'Name': 'always'}
     if passkey is None:
@@ -59,7 +58,9 @@ def apt_start(tag=None, passkey=None):
         'GPG_PUBLIC_KEY_FILE': '/keys/breqwatr-private-key.asc',
     }
     ports = {'80': ('0.0.0.0', '81')}
-    docker.run(image, name='bw-apt', environment=env,
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
+    docker.run(image, name=name, environment=env,
                restart_policy=restart_policy, ports=ports)
 
 
@@ -71,6 +72,8 @@ def pip_start(tag=None):
     docker = Docker()
     docker.pull(repository=repo, tag=tag)
     image = '{}:{}'.format(repo, tag)
+    docker = Docker()
+    docker.pull(repository=repo, tag=tag)
     docker.run(image, name='bw-pip', network_mode='host')
 
 
