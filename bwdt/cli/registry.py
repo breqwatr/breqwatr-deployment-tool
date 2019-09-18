31da16276a3dece5d831ffde1f8438bfd0d5e6ba
kyle.pericak <kyle.pericak@breqwatr.com>
2019-09-18 17:48:06 -0400

    renamed the sync commands

--- bwdt/cli/registry.py
+++ bwdt/cli/registry.py
@@ -25,7 +25,7 @@ def start(ip, port):
 @click.argument('image_name')
 @click.argument('registry_url')
 @click.option('--tag', default=None, help='optional image tag')
-@click.command(name='sync-image')
+@click.command(name='sync-openstack-image')
 def sync_image(image_name, registry_url, tag):
     """ Load image_name and push it to the local registry """
     click.echo('Sync {} to {}...'.format(image_name, registry_url))
@@ -34,7 +34,7 @@ def sync_image(image_name, registry_url, tag):
 
 @click.argument('registry_url')
 @click.option('--tag', default=None, help='optional image tag')
-@click.command(name='sync-all-images')
+@click.command(name='sync-all-openstack-images')
 def sync_all_images(registry_url, tag):
     """ Load all images and push them to the local registry """
     click.echo('Pushing all images to {}'.format(registry_url))
