d0979f4db5a848d0c6852d2cf4f1940b43a6baef
kyle.pericak <kyle.pericak@breqwatr.com>
2019-10-09 14:49:00 -0400

    refactor openstack cli to its own file

--- bwdt/cli/ansible.py
+++ bwdt/cli/ansible.py
@@ -3,6 +3,7 @@ import click
 
 import bwdt.services.ansible as ansible
 import bwdt.cli.ceph
+import bwdt.cli.openstack
 
 
 @click.group(name='ansible')
@@ -39,54 +40,7 @@ def transfer_kolla_dir(server_ip, user):
     click.echo('Done')
 
 
-@click.group()
-def openstack():
-    """ Command group for bwdt ansible openstack """
-
-
-@click.command(name='gen-config')
-def gen_config():
-    """ Generate OpenStack config files in the ansible container """
-    cloud_yml = '-e @/etc/breqwatr/cloud.yml'
-    conn = '-e ansible_connection=local'
-    inv = '-i localhost,'
-    playbook = '/var/repos/bw-ansible/generate-kolla-config.yml'
-    cmd = 'ansible-playbook {} {} {} {}'.format(cloud_yml, conn, inv, playbook)
-    docker_cmd = 'docker exec -it ansible {}'.format(cmd)
-    click.echo(docker_cmd)
-
-
-@click.command()
-def bootstrap():
-    """ Run kolla-ansible bootstrap """
-    cmd = ('docker exec -it ansible kolla-ansible'
-           '-i /etc/kolla/inventory bootstrap-servers')
-    click.echo(cmd)
-
-
-@click.command()
-def deploy():
-    """ Run kolla-ansible deploy """
-    cmd = ('docker exec -it ansible kolla-ansible'
-           '-i /etc/kolla/inventory deploy')
-    click.echo(cmd)
-
-
-@click.command(name='post-deploy')
-def post_deploy():
-    """ Run kolla-ansible post-deploy """
-    cmd = ('docker exec -it ansible kolla-ansible'
-           '-i /etc/kolla/inventory post-deploy')
-    click.echo(cmd)
-
-
 ansible_group.add_command(transfer_kolla_dir)
 ansible_group.add_command(start)
-
-openstack.add_command(gen_config)
-openstack.add_command(bootstrap)
-openstack.add_command(deploy)
-openstack.add_command(post_deploy)
-ansible_group.add_command(openstack)
-
 ansible_group.add_command(bwdt.cli.ceph.ceph_group)
+ansible_group.add_command(bwdt.cli.openstack.openstack_group)
