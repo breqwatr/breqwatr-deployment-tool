a0f6fefcc10902225f1744e0e95ba7f4511a6ada
kyle.pericak <kyle.pericak@breqwatr.com>
2020-03-24 19:47:22 -0400

    corrected openrc path

--- bwdt/lib/openstack.py
+++ bwdt/lib/openstack.py
@@ -150,7 +150,7 @@ def kolla_ansible_get_admin_openrc(release, inventory_path, globals_path,
            + f'-v {cwd}:/target '
            f'{constants.IMAGE_PREFIX}/kolla-ansible:{release} '
            f'bash -c "kolla-ansible post-deploy -i /etc/kolla/inventory && '
-           f'cp /etc/kolla/etc/kolla/admin-openrc.sh /target/"')
+           f'cp /etc/kolla/admin-openrc.sh /target/"')
     docker.shell(cmd)
 
 
